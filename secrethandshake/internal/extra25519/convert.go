425d26ba1c7b811ef55c25cfaa1a8ab1a2fa0704
Henry <cryptix@riseup.net>
2021-07-05 18:08:03 +0200

    remove edwardsToMontgomeryX

--- secrethandshake/internal/extra25519/convert.go
+++ secrethandshake/internal/extra25519/convert.go
@@ -5,7 +5,6 @@ import (
 	"crypto/sha512"
 
 	"filippo.io/edwards25519"
-	"filippo.io/edwards25519/field"
 	"golang.org/x/crypto/ed25519"
 
 	"go.cryptoscope.co/secretstream/internal/lo25519"
@@ -26,21 +25,6 @@ func PrivateKeyToCurve25519(curve25519Private *[32]byte, privateKey ed25519.Priv
 	copy(curve25519Private[:], digest)
 }
 
-func edwardsToMontgomeryX(y *field.Element) *field.Element {
-	// We only need the x-coordinate of the curve25519 point, which I'll
-	// call u. The isomorphism is u=(y+1)/(1-y), since y=Y/Z, this gives
-	// u=(Y+Z)/(Z-Y). We know that Z=1, thus u=(Y+1)/(1-Y).
-
-	var oneMinusY = new(field.Element).One()
-	oneMinusY.Subtract(oneMinusY, y)
-	oneMinusY.Invert(oneMinusY)
-
-	var x = new(field.Element).One()
-	x.Add(x, y)
-	x.Multiply(x, oneMinusY)
-	return x
-}
-
 // PublicKeyToCurve25519 converts an Ed25519 public key into the curve25519
 // public key that would be generated from the same private key.
 func PublicKeyToCurve25519(curve25519Public *[32]byte, publicKey ed25519.PublicKey) bool {
@@ -55,11 +39,6 @@ func PublicKeyToCurve25519(curve25519Public *[32]byte, publicKey ed25519.PublicK
 		return false
 	}
 
-	_, aY, _, _ := a.ExtendedCoordinates()
-	// A.Z = 1 as a postcondition of FromBytes.
-
-	x := edwardsToMontgomeryX(aY)
-
-	copy(curve25519Public[:], x.Bytes())
+	copy(curve25519Public[:], a.BytesMontgomery())
 	return true
 }
